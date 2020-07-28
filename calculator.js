db4e2852a999c89f76b9c6b09c3b0c8a79aaaf9c
PreetiBoddeti <iteerp999@gmail.com>
2020-07-28 18:41:33 +0530

    secnd commit

--- calculator.js
+++ calculator.js
@@ -1,29 +1,78 @@
-alert('dsfbjdfsd')
 const defaultResult=0
 let CurrentResult=defaultResult
 
+//button declaration from html
 const addBtn=document.getElementById("add");
+const subBtn=document.getElementById("sub");
+const mulBtn=document.getElementById("mul");
+const divBtn=document.getElementById("div");
+const modBtn=document.getElementById("mod")
+
 console.log(addBtn)
 //addBtn.addEventListener('click',add)
 
+//user input from the html page
 function getUserInput(){
     userInput=document.getElementById('input').value
-    return userInput
+    return parseInt(userInput)
     
 }
 
-function displayOutput(operator, initialResult, calcResult){
-    document.getElementById('display').innerHTML=`${operator} ${initialResult} ${calcResult}`
+//display the calculated result
+function displayOutput(operator, initialResult,calcResult,inputNumber){
+    console.log(inputNumber)
+    document.getElementById('display').innerHTML="The Calulation is "+`${initialResult} ${operator} ${inputNumber}`
+    document.getElementById('Result').innerHTML="The total Result is "+ `${calcResult}`
 }
 
+//add functionality
 function add(){
     alert( 'Add function')
-    inputNumber=parseInt(getUserInput())
+    const inputNumber=getUserInput()
     const initialResult=CurrentResult
     CurrentResult=CurrentResult+inputNumber
-    displayOutput('+',initialResult,CurrentResult)
+    displayOutput('+',initialResult,CurrentResult,inputNumber)
 }
 
+//subtract functionality
+function sub(){
+    alert( 'Subtract function')
+    inputNumber=parseInt(getUserInput())
+    const initialResult=CurrentResult
+    CurrentResult=CurrentResult-inputNumber
+    displayOutput('-',initialResult,CurrentResult,inputNumber)
+}
 
+//multiplication functionality
+function mul(){
+    alert( 'Multiplication function')
+    inputNumber=parseInt(getUserInput())
+    const initialResult=CurrentResult
+    CurrentResult=CurrentResult*inputNumber
+    displayOutput('*',initialResult,CurrentResult,inputNumber)
+}
+
+//division functionality
+function div(){
+    alert( 'Div function')
+    inputNumber=parseInt(getUserInput())
+    const initialResult=CurrentResult
+    CurrentResult=CurrentResult/inputNumber
+    displayOutput('/',initialResult,CurrentResult,inputNumber)
+}
+
+//modulus functionality
+function mod(){
+    alert( 'Div function')
+    inputNumber=parseInt(getUserInput())
+    const initialResult=CurrentResult
+    CurrentResult=CurrentResult%inputNumber
+    displayOutput('%',initialResult,CurrentResult,inputNumber)
+}
+
+//invoke event listeners
 addBtn.addEventListener('click',add)
-
+subBtn.addEventListener('click',sub)
+mulBtn.addEventListener('click',mul)
+divBtn.addEventListener('click',div)
+modBtn.addEventListener('click',mod)
